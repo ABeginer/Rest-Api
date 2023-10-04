aa3ea78b61aeac3ffba2c5a4cce207f4313ba213
Thuan <thuanton2003@gmail.com>
2023-10-04 21:27:10 +0700

    add delete api to program

--- app.js
+++ app.js
@@ -1,53 +1,57 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 const PORT = 8080;
+let balance = 0;
 
-app.listen(PORT,()=>{
-    console.log(`device is listening on port: ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`device is listening on port: ${PORT}`);
 });
-let fs = require('fs');
-let catData = JSON.parse(fs.readFileSync('./cat-data.json'));
-app.get('/allcat',(req,res)=>{
-    res.status(200).send({
-         catData
-    })
-})
-app.get('/cat',(req, res) => {
-    res.status(200).send({
-        name: 'Alan',
-        species: 'hairless'
-    });
+let fs = require("fs");
+let catData = JSON.parse(fs.readFileSync("./cat-data.json"));
+
+app.get("/showcat", (req, res) => {
+  res.status(200).send({
+    catData,
+  });
 });
-app.get('/cat/:id',(req,res)=>{
-    const {id} = req.params;
-    res.status(200).send(
-       catData[id-1]
-    )
-})
-app.post('/cat',(req,res)=>{
-    const{id} = req.params;
-    const{species} = req.body;
-    if(!species){
-        res.status(418).send({
-            message: 'you need to input species for the cat'
-        })
-    } 
-    res.send({
-        cat: `your cat belong to: ${species} and have ID of ${id}`
-    })
-})
-app.post('/addcat',(req,res)=>{
-    const newID = catData[catData.length-1].id + 1;
-    const newCat = Object.assign({id: newID},req.body);
-    catData.push(newCat);
-    fs.writeFile('./cat-data.json',JSON.stringify(catData),(err)=>{
-        res.status(201).json({
-            status: "Cat added",
-            data:{
-                "cat": newCat
-            }
-        })
+
+app.get("/showcat/:id", (req, res) => {
+  const { id } = req.params;
+  res.status(200).send(catData[id - 1]);
+});
+
+app.post("/buycat", (req, res) => {
+  const newID = catData[catData.length - 1].id + 1;
+  const newCat = Object.assign(req.body, { id: newID });
+  balance -= newCat.price;
+  catData.push(newCat);
+  fs.writeFile("./cat-data.json", JSON.stringify(catData), (err) => {
+    res.status(201).json({
+      status: `Cat named: ${newCat.name} added`,
+      data: {
+        balance: balance,
+        cat: newCat,
+      },
     });
-  })
-module.exports = app
+  });
+});
+app.delete("/sellcat/:id", (req, res) => {
+  const { id } = req.params;
+  let soldCat = catData[id - 1];
+  balance += catData[id - 1].price;
+  for (let i = id; i < catData.length; i++) {
+    catData[i].id = i;
+  }
+  
+  catData.splice(id - 1, 1);
+
+  fs.writeFile("./cat-data.json", JSON.stringify(catData), (err) => {
+    res.status(200).json({
+      status: "Cat sold",
+      balance: balance,
+      soldcat: soldCat,
+    });
+  });
+});
+module.exports = app;
